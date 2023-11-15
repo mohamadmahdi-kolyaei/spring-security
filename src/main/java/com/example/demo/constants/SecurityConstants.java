f5c1541eeacd7cad027caf00f0c0a81272a24d18
mohamad mahdi kolyaei <mohamadmahdikolyaei@gmail.com>
2023-11-16 00:27:58 +0330

    delete security constant

--- src/main/java/com/example/demo/constants/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.constants;
-
-public interface SecurityConstants {
-    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-    // The secret key is typically a long, random, and secret string that you use to sign and verify JWTs. It's crucial to keep this key secret and secure.
-    public static final String JWT_HEADER = "Authorization";
-}
